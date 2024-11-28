85a9f024422262dfa2de59f9c869f5c0a3cdfa67
vvbakhanovich <vv.bakh@yandex.ru>
2024-11-28 11:49:23 +0300

    feat: Add swagger

--- src/main/java/ru/ms/second/team/registration/dto/request/NewRegistrationDto.java
+++ src/main/java/ru/ms/second/team/registration/dto/request/NewRegistrationDto.java
@@ -1,21 +1,31 @@
 package ru.ms.second.team.registration.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.*;
 import lombok.Builder;
 
 @Builder
+@Schema(name = "New registration data")
 public record NewRegistrationDto(
+
         @NotBlank(message = "Username cannot be blank")
+        @Schema(description = "Author's username")
         String username,
+
         @Email
         @NotBlank
         @Size(min = 6, max = 254, message = "Email's length cannot be less than 6 and more than 254 symbols")
+        @Schema(description = "Author's email")
         String email,
+
         @Pattern(regexp = "7\\d{10}", message = "Phone Number is Incorrect")
         @NotNull
+        @Schema(description = "Author's phone")
         String phone,
+
         @Positive
         @NotNull
+        @Schema(description = "Event id")
         Long eventId
 ) {
 }
--- src/main/java/ru/ms/second/team/registration/dto/request/RegistrationCredentials.java
+++ src/main/java/ru/ms/second/team/registration/dto/request/RegistrationCredentials.java
@@ -1,5 +1,6 @@
 package ru.ms.second.team.registration.dto.request;
 
+import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Positive;
@@ -7,12 +8,17 @@ import jakarta.validation.constraints.Size;
 import lombok.Builder;
 
 @Builder
+@Schema(description = "Registrations credentials")
 public record RegistrationCredentials(
+
         @Positive
         @NotNull
+        @Schema(description = "Registration id")
         Long id,
+
         @NotBlank
         @Size(min = 4, max = 4, message = "Password must contain 4 symbols")
+        @Schema(description = "Author's username")
         String password
 ) {
 }
